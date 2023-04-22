e91869737ad65af18afe78214bf9300c3fb2d880
penny1022 <penny7418521@gmail.com>
2023-04-23 02:53:54 +0800

    add datasetlist

---
+++ src/pages/datasetlist/components/hearder.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>AI&nbsp;Data&nbsp;Sharing</div>
+      <div>
+        <button><a href="edit/newDatset">新增資料集</a></button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/datasetlist/components/list.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <div>
+      <h2>A_DATA_SET</h2>
+      <p>description</p>
+    </div>
+  );
+};
+
+export default List;
