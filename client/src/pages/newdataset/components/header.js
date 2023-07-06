6d636e1e71eb514614b5d31b3db8ba4c9c7fd00b
penny1022 <105259467+penny1022@users.noreply.github.com>
2023-07-06 15:22:22 +0800

    newdataset header

---
+++ client/src/pages/newdataset/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="head">
+        <div>
+          <img src="LOGO.svg" className="logo1"></img>
+          <h1 className="text">新建資料集</h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
