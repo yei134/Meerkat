0077a8fd00316fd48952f33cad02af5e789d3975
sz <1061433@gmail.com>
2023-10-25 18:26:02 +0800

    feat: header>Add related website shortcuts and fine-tune the layout position
    Added icons and shortcuts for CYLab's GitHub and YNCU's CKAN, changed the platform icon and added a link to the personnel management icon, and fine-tuned the login page display.

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,6 +1,11 @@
-import { useKeycloak } from "@react-keycloak/web";
-import "./index.css";
+// 套件
 import { useState } from "react";
+import { useKeycloak } from "@react-keycloak/web";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+// 檔案
+import "./index.css";
+
 export default function Header() {
   const { keycloak } = useKeycloak();
   const [userInfo, setUserInfo] = useState({});
@@ -21,17 +26,28 @@ export default function Header() {
     <div className="header">
       <div className="headerLeft">
         <a href="/">
-          <img src="/LOGO.svg" className="headerLeftLogo" />
+          <img src="/MeerkatTitle.svg" className="headerLeftLogo" />
+        </a>
+        <a href="https://github.com/cylab-tw" target="_blank">
+          <GitHubIcon sx={{ fontSize: "1.5rem", alignSelf: "flex-end", margin: "0.2rem" }} />
+        </a>
+        <a href="	https://data.dmc.nycu.edu.tw/" target="_blank">
+          <img src="/NYCU_icon.png" alt="NYCU" className="otherIcon" />
         </a>
       </div>
       <div className="headerRight">
         {keycloak.authenticated ? (
-          <button
-            className="headerRightButton"
-            onClick={() => {
-              keycloak.logout();
-            }}
-          >{`(${userInfo.email !== undefined ? userInfo.email.split("@")[0] : ""}) Logout`}</button>
+          <>
+            <button
+              className="headerRightButton"
+              onClick={() => {
+                keycloak.logout();
+              }}
+            >{`Logout (${userInfo.email !== undefined ? userInfo.email.split("@")[0] : ""})`}</button>
+            <a href="/members">
+              <ManageAccountsIcon sx={{ fontSize: "1.8rem" }} />
+            </a>
+          </>
         ) : (
           <button
             className="headerRightButton"
